3ed6f964394557a3086d2c3abecab530728a25c7
loveyunk <mjjccc@126.com>
2020-05-01 13:41:05 +0800

    update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://my-json-server.typicode.com/loveyunk/vue-tailwind-user-dashboard/'
+    : 'http://localhost:3000';
+
 const service = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
